72a05220b6529e6551ca93235ac5deb01dbf7856
Sai <9091157+SaiG18@users.noreply.github.com>
2018-12-15 11:06:01 -0500

    index.js

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 function logout(){
-    /*
-    window.location.href = 'http://www.google.com';
-    const websites = {};
-    window.location.href = "https://www.google.com/accounts/logout";
-    window.location.href = "http://example.com";
-    window.open("http://www.google.com",'_blank');
-     var req = new XMLHttpRequest();
-    req.open('GET', "https://github.com/logout", true);
-     const finreq = new XMLfinreqRequest();
-    const url='https://jsonplaceholder.typicode.com/posts';
-    finreq.open("GET", url);
-    finreq.send();
-    finreq.onreadystatechange=(e)=>{
-    console.log(finreq.responseText);
-     inF = inF.contentWindow;
-    inF.contentDocument.document;
-    inF.contentDocument;
-    inF.document.open();
-    inF.document.write('Test');
-    inF.document.close();
-  }
-   */
+
      var sites = ['http://www.amazon.com/gp/flex/sign-out.html?action=sign-out',
     'https://www.blogger.com/logout.g',
     //'https://www.delicious.com/logout',
@@ -43,9 +22,10 @@ function logout(){
     'https://login.yahoo.com/config/login?.src=fpctx&logout=1&.direct=1&.done=http://www.yahoo.com/',
     'https://www.youtube.com',
     'https://google.com/accounts/logout',
-  ];
-     var len = sites.length;
-     for(var i = 0; i < len; i++) {
+    ];
+
+    var len = sites.length;
+    for(var i = 0; i < len; i++) {
     var frameEmbed = document.createElement("iframe");
     frameEmbed.setAttribute("src", sites[i]);
     frameEmbed.style.width = "0";
@@ -56,18 +36,13 @@ function logout(){
     //window.alert("Done!");
      }
     
-     console.log("Done!")
-    
+   
     swal({
     icon: "success",
     text: "Success!",
     timer: 3000,
     button: false
-  });
+    });
 
-
-     /*var win = window.open("http://www.amazon.com/gp/flex/sign-out.html?action=sign-out", '1366002941508','width=500,height=200,left=375,top=330');
-     setTimeout(function () { win.close();}, 2000);
-     */
    }
 
